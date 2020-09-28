0b569a287e6275c0308030c95720e962f7f58fd8
ericerodgers <ericerodgers@yahoo.com>
2020-09-28 15:22:46 -0400

    all changes made and finalized

--- app.js
+++ app.js
@@ -15,15 +15,48 @@
         { text: 'Nice Nice Nice!', id: 542328 }
       ];
   
-      // Some and Every Checks
+      // Some and Every Checks - these return a boolean true or false
       // Array.prototype.some() // is at least one person 19 or older?
+      const isAdult = people.some(function(person) {
+        const currentYear = (new Date()).getFullYear();
+        if(currentYear - person.year >= 19) {
+            return true;
+        }
+      });
+      //or:
+      const isAdult2 = people.some(person => {
+        const currentYear = (new Date()).getFullYear();
+        return currentYear - person.year >= 19;
+      });
+      //or:
+      const isAdult3 = people.some(person => ((new Date())
+            .getFullYear()) - person.year >= 19 );
+
       // Array.prototype.every() // is everyone 19 or older?
+      //Same as above just change to .every
+      const allAdults = people.every(person => ((new Date())
+      .getFullYear()) - person.year >= 19 );
   
       // Array.prototype.find()
       // Find is like filter, but instead returns just the one you are looking for
       // find the comment with the ID of 823423
+      const comment = comments.find(function(comment) {
+          if(comment.id === 823423) {
+              return true;
+          }
+      });
+      //or:
+      const comment = comments.find(comment => comment.id === 823423);
   
       // Array.prototype.findIndex()
       // Find the comment with this ID
       // delete the comment with the ID of 823423
-  
+      const index = comments.findIndex(comment => comment.id === 823423);
+
+      comments.splice(index, 1);
+      //above removes the comment from the original array
+      //or you can do the below method to make new comments array:
+      const newComments = [
+          ...comments.slice(0, index),
+          ...comments.slice(index + 1)
+      ];
